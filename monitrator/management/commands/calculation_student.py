b5a721188f1e9dc8b82902ef83d696e49598f51b
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2023-08-13 20:49:25 +0530

    calc

--- monitrator/management/commands/calculation_student.py
+++ monitrator/management/commands/calculation_student.py
@@ -1,49 +1,63 @@
 from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
-from monitrator.models import Store, StoreStatus, MenuHours
+from monitrator.models import Store, MenuHours
 
 class Command(BaseCommand):
     help = 'Calculate uptime for stores'
 
     def handle(self, *args, **options):
         now = datetime.utcnow()
-        last_hour = now - timedelta(hours=1)
+        last_hour = now - timedelta(minutes=60)  # Convert last hour to minutes
         last_day = now - timedelta(days=1)
         last_week = now - timedelta(weeks=1)
 
         all_stores = Store.objects.all()
-        
+
         for store in all_stores:
             store_id = store.store_id
-            active = timedelta(seconds=0)  # Initialize active time
+            total_active_time = 0  # Initialize total active time
+            total_active_last_day = timedelta(seconds=0)
+            total_active_last_week = timedelta(seconds=0)
+            downtime_last_hour = 0  # Initialize downtime_last_hour
 
             business_hours = MenuHours.objects.filter(store_id=store_id)
-            
+
             for business_hour in business_hours:
                 start_time_local = business_hour.start_time_local
                 end_time_local = business_hour.end_time_local
-                
-                if start_time_local <= now <= end_time_local:
-                    active += min(now, end_time_local) - max(start_time_local, last_hour)
-            
-            total_active_time = active.total_seconds()
-            print(f"Store ID: {store_id}, Total Active Time (last hour): {total_active_time} seconds")
-   
-            if datetime.now > business_hour.start_time_local > last_day:
-                    uptime_last_day = datetime.now - business_hour.start_time_local
-                    if datetime.now > business_hour.end_time_local:
-                        downtime_last_day = datetime.now-business_hour.end_time_local
-                        active_last_day = uptime_last_day-downtime_last_day
+
+                start_datetime_local = datetime.combine(datetime.today(), start_time_local)
+                end_datetime_local = datetime.combine(datetime.today(), end_time_local)
+
+                if start_datetime_local <= now <= end_datetime_local:
+                    active = min(now, end_datetime_local) - max(start_datetime_local, last_hour)
+                    total_active_time += active.total_seconds()
+                    downtime_last_hour = (3600 - total_active_time)
+
+                if last_day < start_datetime_local <= now:
+                    uptime_last_day = now - start_datetime_local
+                    if now > end_datetime_local:
+                        downtime_last_day = now - end_datetime_local
+                        active_last_day = uptime_last_day - downtime_last_day
                     else:
-                        active_last_day = datetime.now-uptime_last_day
-                total = total+active_last_day
-            print(total)
-            if datetime.now > business_hour.start_time_local > last_week:
-                    uptime_last_week = datetime.now - business_hour.start_time_local
-                    if datetime.now > business_hour.end_time_local:
-                        downtime_last_week = datetime.now-business_hour.end_time_local
-                        active_llast_week = uptime_last_week-downtime_last_week
+                        active_last_day = now - uptime_last_day
+                    total_active_last_day += active_last_day
+                    downtime_last_day = (24 * 3600) - total_active_last_day.total_seconds()
+
+                if last_week < start_datetime_local <= now:
+                    uptime_last_week = now - start_datetime_local
+                    if now > end_datetime_local:
+                        downtime_last_week = now - end_datetime_local
+                        active_last_week = uptime_last_week - downtime_last_week
                     else:
-                        active_last_week = datetime.now-uptime_last_week
-                total = total+active_last_week
-            print(total)
+                        active_last_week = now - uptime_last_week
+                    total_active_last_week += active_last_week
+
+            downtime_last_week = (7 * 24 * 3600) - total_active_last_week.total_seconds()
+
+            print(f"Store ID: {store_id}, Total Active Time (last hour): {total_active_time / 60:.2f} minutes")
+            print(f"Downtime Last Hour: {downtime_last_hour / 60:.2f} minutes")
+            print(f"Total Active Time (last day): {total_active_last_day.total_seconds() / 3600:.2f} hours")
+            print(f"Downtime Last Day: {downtime_last_day / 3600:.2f} hours")
+            print(f"Total Active Time (last week): {total_active_last_week.total_seconds() / 3600:.2f} hours")
+            print(f"Downtime Last Week: {downtime_last_week / (7 * 24 * 3600):.2f} hours")
--- monitrator/management/commands/calculations.py
+++ monitrator/management/commands/calculations.py
@@ -15,63 +15,65 @@ class Command(BaseCommand):
             store_id = store.store_id
 
             business_hours = MenuHours.objects.filter(store_id=store_id)
-            status = StoreStatus.objects.filter(store_id=store_id)
 
             total_active = timedelta()  # Initialize total active time
-            hours_in_day = 0
-            day_max = -1
+            total_active_last_day = timedelta(seconds=0)
+            total_active_last_week = timedelta(seconds=0)
+            downtime_last_hour = 0  # Initialize downtime_last_hour
 
             for business_hour in business_hours:
-                day = business_hour.day
-                day_max = max(day_max, day)
-                start_time_local = datetime.combine(datetime.today(), business_hour.start_time_local)
-                end_time_local = datetime.combine(datetime.today(), business_hour.end_time_local)
+                start_time_local = business_hour.start_time_local
+                end_time_local = business_hour.end_time_local
 
-                if end_time_local < start_time_local:
-                    # Handle case where end time is before start time (crosses midnight)
-                    end_time_local += timedelta(days=1)
+                start_datetime_local = datetime.combine(datetime.today(), start_time_local)
+                end_datetime_local = datetime.combine(datetime.today(), end_time_local)
 
-                active_duration = end_time_local - start_time_local  # Calculate active duration
+                if start_datetime_local <= now <= end_datetime_local:
+                    active = min(now, end_datetime_local) - max(start_datetime_local, last_hour)
+                    total_active += active
+                    downtime_last_hour = (3600 - total_active.total_seconds())
 
-                # Calculate total active time for this business hour
-                active_duration_total_seconds = active_duration.total_seconds()
-                active_duration_total_hours = active_duration_total_seconds / 3600  # Convert seconds to hours
+                if last_day < start_datetime_local <= now:
+                    uptime_last_day = now - start_datetime_local
+                    if now > end_datetime_local:
+                        downtime_last_day = now - end_datetime_local
+                        active_last_day = uptime_last_day - downtime_last_day
+                    else:
+                        active_last_day = now - uptime_last_day
+                    total_active_last_day += active_last_day
+                    downtime_last_day = (24 * 3600) - total_active_last_day.total_seconds()
 
-                if day == day_max:
-                    hours_in_day = active_duration_total_hours
+                if last_week < start_datetime_local <= now:
+                    uptime_last_week = now - start_datetime_local
+                    if now > end_datetime_local:
+                        downtime_last_week = now - end_datetime_local
+                        active_last_week = uptime_last_week - downtime_last_week
+                    else:
+                        active_last_week = now - uptime_last_week
+                    total_active_last_week += active_last_week
 
-                total_active += timedelta(hours=active_duration_total_hours)
+            downtime_last_week = (7 * 24 * 3600) - total_active_last_week.total_seconds()
 
-            # Calculate other metrics
-            uptime_last_hour = hours_in_day * 60
-            uptime_last_day = total_active.total_seconds() / 3600
-            uptime_last_week = total_active.total_seconds() / 3600
-
-            # Calculate downtime for the last hour (considering time passed within the current hour)
-            downtime_last_hour = (60 - uptime_last_hour) if hours_in_day >= 1 else 60
-
-            downtime_last_day = 24 - uptime_last_day  # Correct calculation
-            downtime_last_week = 7 * 24 - uptime_last_week  # Correct calculation
             metrics = StoreMetrics(
                 store=store,
-                uptime_last_hour=uptime_last_hour,
-                uptime_last_day=uptime_last_day,
-                uptime_last_week=uptime_last_week,
-                downtime_last_hour=downtime_last_hour,
-                downtime_last_day=downtime_last_day,
-                downtime_last_week=downtime_last_week
+                uptime_last_hour=total_active.total_seconds() / 60,  # Convert to minutes
+                uptime_last_day=total_active_last_day.total_seconds() / 3600,  # Convert to hours
+                uptime_last_week=total_active_last_week.total_seconds() / 3600,  # Convert to hours
+                downtime_last_hour=downtime_last_hour / 60,  # Convert to minutes
+                downtime_last_day=downtime_last_day / 3600,  # Convert to hours
+                downtime_last_week=downtime_last_week / 3600  # Convert to hours
             )
             metrics.save()
 
             self.stdout.write(f"Store ID: {store_id}")
-            self.stdout.write(f"Uptime Last Hour: {uptime_last_hour:.2f} minutes")
-            self.stdout.write(f"Uptime Last Day: {uptime_last_day:.2f} hours")
-            self.stdout.write(f"Uptime Last Week: {uptime_last_week:.2f} hours")
-            self.stdout.write(f"Downtime Last Hour: {downtime_last_hour:.2f} minutes")
-            self.stdout.write(f"Downtime Last Day: {downtime_last_day:.2f} hours")
-            self.stdout.write(f"Downtime Last Week: {downtime_last_week:.2f} hours")
+            self.stdout.write(f"Uptime Last Hour: {total_active.total_seconds() / 60:.2f} minutes")
+            self.stdout.write(f"Uptime Last Day: {total_active_last_day.total_seconds() / 3600:.2f} hours")
+            self.stdout.write(f"Uptime Last Week: {total_active_last_week.total_seconds() / 3600:.2f} hours")
+            self.stdout.write(f"Downtime Last Hour: {downtime_last_hour / 60:.2f} minutes")
+            self.stdout.write(f"Downtime Last Day: {downtime_last_day / 3600:.2f} hours")
+            self.stdout.write(f"Downtime Last Week: {downtime_last_week / 3600:.2f} hours")
             self.stdout.write("-" * 50)
-        
+
         # Define CSV filename
         csv_filename = "store_metrics.csv"
 
@@ -89,5 +91,5 @@ class Command(BaseCommand):
                     store.uptime_last_week, store.downtime_last_hour,
                     store.downtime_last_day, store.downtime_last_week
                 ])
-        
+
         self.stdout.write(f"CSV file '{csv_filename}' created successfully.")
